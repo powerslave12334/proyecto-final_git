769a49528a1152e0db478b39f4c54714df088332
powerslave12334 <hideonlawea@gmail.com>
2025-11-07 16:40:52 -0600

    modificaciones paralelas en la rama principal

--- app.js
+++ app.js
@@ -7,3 +7,23 @@ const segundos = document.getElementById("segundos");
 minutos.textContent = m;
 hora.textContent = h;
 segundos.textContent = s;
+
+setInterval(function(){
+  if (s === 59){
+    s = -1; // Para que al final del bloque termine siendo 0
+    m++;
+    minutos.textContent = m;
+  }
+  if (m === 60){
+    m = 0;
+    h++;
+    hora.textContent = h;
+    minutos.textContent = m;
+  }
+  if(h === 24){
+    h = 0;
+    hora.textContent = h;
+  }
+  s++;
+  segundos.textContent = s;
+}, 1000);
--- funciones.js
+++ funciones.js
@@ -1,11 +1,20 @@
 function seconds(string) {
-   
+    for (let seg = 0; seg <= 59; seg++) {
+        console.log(`${string} ${seg} seg`);
+    }
 }
 
 function minutes(string) {
-    
+    for (let min = 0; min <= 59; min++) {
+        seconds(`${string }${min} min`);
+    }
 }
 
 function hours() {
-   
+    let string = "";
+    for (let hour = 0; hour <= 23; hour++) {
+        string = `${hour} hrs `
+        minutes(string);
+        string = "";
+    }
 }
