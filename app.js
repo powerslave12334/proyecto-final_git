61f780184f949a03273227c28a386547894b7d88
powerslave12334 <hideonlawea@gmail.com>
2025-11-07 16:32:35 -0600

    Preparacion del entorno

---
+++ app.js
@@ -0,0 +1,9 @@
+var h = 0;
+var m = 0;
+var s = 0;
+const hora = document.getElementById("hora");
+const minutos = document.getElementById("minutos");
+const segundos = document.getElementById("segundos");
+minutos.textContent = m;
+hora.textContent = h;
+segundos.textContent = s;
---
+++ funciones.js
@@ -0,0 +1,11 @@
+function seconds(string) {
+   
+}
+
+function minutes(string) {
+    
+}
+
+function hours() {
+   
+}
